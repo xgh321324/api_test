b142bd81cca50c8b3ffee361fb2725473bd5d340
xgh321324 <34511103+xgh321324@users.noreply.github.com>
2018-05-09 16:58:26 +0800

    澜渟医生基础信息的一些接口新增

---
+++ case/BasicInfo/testDoctorInfo.py
@@ -0,0 +1,72 @@
+#coding:utf-8
+import requests,unittest
+from common.login import LG
+from common.logger import Log
+
+class Doctor(unittest.TestCase):
+
+    def setUp(self):
+        self.log = Log()
+        self.s = requests.session()
+        self.lgin = LG() #实例化登录类
+        self.uid_token = self.lgin.gettoken_loginbyUID() #直接取第二部登录
+        self.header = {'User-Agent': 'LanTingDoctor/1.3.1 (iPad; iOS 10.1.1; Scale/2.00)',
+                       'Accept-Encoding': 'gzip, deflate',
+                       'Accept-Language': 'zh-Hans-CN;q=1',
+                       'Content-Type': 'application/json',
+                       'requestApp': '3',
+                       'requestclient': '2',
+                       'versionForApp': '2.0',
+                       'Authorization': 'Basic YXBpTGFudGluZ0BtZWRsYW5kZXIuY29tOkFwaVRobWxkTWxkQDIwMTM=',
+                       'Connection': 'keep-alive'
+                       }
+
+    def test_update_good_at(self):
+        u'更新专业擅长接口'
+        '''专业擅长内容：10 至 512 个字符'''
+        self.log.info('开始测试更新专业擅长接口')
+
+        url = 'http://api.rih.medohealth.com/API/V1/DoctorInfo/modifyRep'
+        content = [' ','08152','!@#$%^&~!@#$%^&*！@###@@','专业擅长吹牛a皮看电视手机立刻就','1asdddsa23WEsjkla在卡利姆多卢']
+
+        for i in content:
+            json_data = {"token":self.uid_token,
+                             "rep":i
+                             }
+            r = self.s.post(url,headers = self.header,json=json_data)
+            if len(i) < 10:
+                self.assertEqual('请输入 10 至 512 个字符.',r.json()['msg'],msg='更新擅长内容失败')
+            else:
+                self.assertEqual('Success.',r.json()['msg'],msg='更新擅长内容失败')
+
+        self.log.info('测试更新专业擅长接口结束！')
+
+
+    def test_update_person_info(self):
+        u'更新个人简介接口'
+        '''简介内容：10 至 512 个字符'''
+        self.log.info('开始测试更新个人简介接口')
+        url = 'http://api.rih.medohealth.com/API/V1/DoctorInfo/modifyRemark'
+        content = ['长度<10','长度等于十88888',
+                   '长度大于10撒了很多哈桑的好阿三的快乐就到拉萨就叫',
+                   '！@@#￥￥%&**……%……&*……&（是否合适2212WEJash'
+                   ]
+
+        for i in content:
+            json_data = {"token":self.uid_token,
+                             "remark":i
+                             }
+            r = self.s.post(url,headers = self.header,json=json_data)
+            if len(i) < 10:
+                self.assertEqual('请输入 10 至 512 个字符.',r.json()['msg'],msg='更新个人简介失败')
+            else:
+                self.assertEqual('Success.',r.json()['msg'],msg='更新个人简介失败')
+
+        self.log.info('测试更新个人简介接口结束')
+
+
+    def tearDown(self):
+        self.s.close()
+
+if __name__=='__main__':
+    unittest.main()
---
+++ case/BasicInfo/testLoginByWechat.py
@@ -0,0 +1,73 @@
+#coding:utf-8
+import requests,unittest
+from common.login import LG
+from common.logger import Log
+
+class Wechat_login(unittest.TestCase):
+    '''
+    APP 编号：澜渟 - 1，澜渟私教 - 2，澜婷医生 - 3；
+    客户端类型编号：Android - 1，iOS - 2，PC - 3，Web - 4;
+    '''
+
+    def setUp(self):
+        self.log = Log()
+        self.s = requests.session()
+        self.lgin = LG() #实例化登录类
+        self.uid_token = self.lgin.gettoken_loginbyUID() #直接取第二部登录
+        self.header1 = {'User-Agent': 'LanTingDoctor/1.3.1 (iPad; iOS 10.1.1; Scale/2.00)',
+                       'Accept-Encoding': 'gzip, deflate',
+                       'Accept-Language': 'zh-Hans-CN;q=1',
+                       'Content-Type': 'application/json',
+                       'requestApp': '3',
+                       'requestclient': '2',
+                       'versionForApp': '2.0',
+                       'Authorization': 'Basic YXBpTGFudGluZ0BtZWRsYW5kZXIuY29tOkFwaVRobWxkTWxkQDIwMTM=',
+                       'Connection': 'keep-alive'
+                       }
+
+    def test_login_by_wechat(self):
+        u'测试澜渟医生IOS微信登录接口'
+        self.log.info('澜渟医生IOS微信登录接口测试开始')
+        url = 'http://api.rih.medohealth.com/API/V1/DoctorLoginForToken/doctorLoginByWeChat'
+        json_data = {"WeChatOpenID":"ogCzv1U6KPBJxRu0-LgrHBhRiJpI",
+                     "WeChatUnionID":"oh3fH06yJfDB6smX6kJACRNlglVY"
+                     }
+        r = self.s.post(url,headers = self.header1,json=json_data)
+        self.assertEqual('登录成功',r.json()['msg'],msg='澜渟医生IOS微信方式登录失败')
+
+        self.log.info('澜渟医生IOS微信登录接口测试结束')
+
+    def test_login_by_wechat2(self):
+        u'澜渟医生测试Andriod微信登录接口'
+        self.log.info('澜渟医生Andriod微信登录接口测试开始')
+        self.header1['requestclient'] = str(1)
+        url = 'http://api.rih.medohealth.com/API/V1/DoctorLoginForToken/doctorLoginByWeChat'
+        json_data = {"WeChatOpenID":"ogCzv1U6KPBJxRu0-LgrHBhRiJpI",
+                     "WeChatUnionID":"oh3fH06yJfDB6smX6kJACRNlglVY"
+                     }
+        r = self.s.post(url,headers = self.header1,json=json_data)
+        self.assertEqual('登录成功',r.json()['msg'],msg='澜渟医生Andriod微信方式登录失败')
+
+        self.log.info('澜渟医生Andriod微信登录接口测试结束')
+
+    def test_login_by_wechat3(self):
+        u'澜渟测试Andriod微信登录接口'
+        self.header1['requestApp'] = str(1)
+        self.log.info('澜渟Andriod微信登录接口测试开始')
+        url = 'http://api.rih.medohealth.com/API/V1/DoctorLoginForToken/doctorLoginByWeChat'
+        json_data = {"WeChatOpenID":"ogCzv1U6KPBJxRu0-LgrHBhRiJpI",
+                     "WeChatUnionID":"oh3fH06yJfDB6smX6kJACRNlglVY"
+                     }
+        r = self.s.post(url,headers = self.header1,json=json_data)
+        self.assertEqual('登录成功',r.json()['msg'],msg='澜渟Andriod微信方式登录失败')
+
+        self.log.info('澜渟Andriod微信登录接口测试结束')
+
+
+    def tearDown(self):
+        self.s.close()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
